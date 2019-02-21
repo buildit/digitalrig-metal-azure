566f31045facd14dc1f44d4639e09af9eb2d2c58
Alfredo Pedraza <alfredopedraza@AlfredoPedraza.local>
2019-02-21 15:55:09 -0600

    Add missing parameters to ther create-parameters.sh script

--- deploy.sh
+++ deploy.sh
@@ -6,29 +6,23 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation> -e <environmentName>" 1>&2; exit 1; }
 
 declare subscriptionId=""
 declare resourceGroupName=""
 declare deploymentName=""
 declare resourceGroupLocation=""
+declare environmentName=""
 
 # Initialize parameters specified from command line
-while getopts ":i:g:n:l:" arg; do
+while getopts ":i:g:n:l:e:" arg; do
 	case "${arg}" in
-		i)
-			subscriptionId=${OPTARG}
-			;;
-		g)
-			resourceGroupName=${OPTARG}
-			;;
-		n)
-			deploymentName=${OPTARG}
-			;;
-		l)
-			resourceGroupLocation=${OPTARG}
-			;;
-		esac
+		i) subscriptionId=${OPTARG} ;;
+		g) resourceGroupName=${OPTARG} ;;
+		n) deploymentName=${OPTARG} ;;
+		l) resourceGroupLocation=${OPTARG} ;;
+		e) environmentName=${OPTARG} ;;
+	esac
 done
 shift $((OPTIND-1))
 
@@ -37,7 +31,6 @@ if [[ -z "$subscriptionId" ]]; then
 	echo "Your subscription ID can be looked up with the CLI using: az account show --out json "
 	echo "Enter your subscription ID:"
 	read subscriptionId
-	[[ "${subscriptionId:?}" ]]
 fi
 
 if [[ -z "$resourceGroupName" ]]; then
@@ -45,7 +38,6 @@ if [[ -z "$resourceGroupName" ]]; then
 	echo "You can create new resource groups with the CLI using: az group create "
 	echo "Enter a resource group name"
 	read resourceGroupName
-	[[ "${resourceGroupName:?}" ]]
 fi
 
 if [[ -z "$deploymentName" ]]; then
@@ -56,30 +48,34 @@ fi
 if [[ -z "$resourceGroupLocation" ]]; then
 	echo "If creating a *new* resource group, you need to set a location "
 	echo "You can lookup locations with the CLI using: az account list-locations "
-	
 	echo "Enter resource group location:"
 	read resourceGroupLocation
 fi
 
+if [[ -z "$environmentName" ]]; then
+	echo "Enter a name of the environment for this deployment:"
+	read environmentName
+fi
+
+if [ -z "$subscriptionId" ] || [ -z "$resourceGroupName" ] || [ -z "$deploymentName" ] || [ -z "$environmentName" ]; then
+	echo "Either one of subscriptionId, resourceGroupName, deploymentName or environmentName is empty."
+	usage
+fi
+
 #templateFile Path - template file to be used
 templateFilePath="template.json"
-
 if [ ! -f "$templateFilePath" ]; then
 	echo "$templateFilePath not found"
 	exit 1
 fi
 
 #parameter file path
-parametersFilePath="parameters.json"
+sh ./create-parameters.sh $environmentName
+parametersFilePath="parameters.$environmentName.json"
 
 if [ ! -f "$parametersFilePath" ]; then
 	echo "$parametersFilePath not found"
 	exit 1
-fi
-
-if [ -z "$subscriptionId" ] || [ -z "$resourceGroupName" ] || [ -z "$deploymentName" ]; then
-	echo "Either one of subscriptionId, resourceGroupName, deploymentName is empty"
-	usage
 fi
 
 #login to azure using your credentials
