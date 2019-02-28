5bc22ce54ae1470856395ebf28a28c364137feb6
Alfredo Figueroa <alfredo.figueroa@wipro.com>
2019-02-28 10:18:10 -0600

    Moving some files to a legacy folder.

---
+++ Makefile
@@ -0,0 +1,15 @@
+create-build-notification:
+	@echo "Create Build Notification"
+	@scripts/create-build-notification.sh
+
+create-build-dashboard:
+	@echo "Create Build Dashboard"
+	@scripts/create-build-dashboard.sh
+
+create-parameters-file:
+	@echo "Create Parameters File"
+	@scripts/create-parameters-file.sh DEV
+
+create-resource-group:
+	@echo "Create Resource Group"
+	@scripts/create-resource-group.sh
---
+++ scripts/create-build-dashboard.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#BUILD DASHBOARD
+#username
+#password
+#api hook url
+#project name & "dashboard" first value in json
+#\
+#
+#
+#@curl -u nicholas.galazzo@wipro.com:hhklxwv6nuxkdiekpdeqnfkkqgr2u2snmpvf2brhheh4qateoqdq \
+#https://dev.azure.com/rigtest/WebContainerTest/WebContainerTestTeam/_apis/dashboard/dashboards?api-version=5.0-preview.2 \
+#-H "Accept: application/json" \
+#-H "Content-type: application/json" \
+#-X POST \
+#-d "@templates/build-dashbard.json"
