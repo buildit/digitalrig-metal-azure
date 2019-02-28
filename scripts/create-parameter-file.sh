02d277fb9cd36656b168bb974f16141d532e3034
David Burkhalter <d2.burkhalter@gmail.com>
2019-02-28 15:44:46 -0600

    added pipeline parameters to create-parameters-file.sh

---
+++ scripts/create-parameter-file.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# Set the default values for the parameter variables.
+DEFAULT_CONTAINER_APP_NAME="RigContainerApp"
+DEFAULT_CONTAINER_PLAN_NAME="RigContainerPlan"
+DEFAULT_CONTAINER_REGISTRY_NAME="RigContainerRegistry"
+DEFAULT_WEB_CONFIG_NAME="RigWebAppConfig"
+DEFAULT_HOST_NAME_BINDING="rigcontainerapp.azurewebsites.net"
+DEFAULT_APP_INSIGHTS_NAME="RigAppInsights"
+
+DEFAULT_DATABASE_LOCATION_NAME="Central US"
+DEFAULT_DATABASE_SERVER_NAME="***REMOVED***testservername"
+DEFAULT_DATABASE_DB_NAME="testdatabase"
+DEFAULT_DATABASE_ADMIN_USERNAME="azuresqladmin"
+DEFAULT_DATABASE_ADMIN_PASSWORD="***REMOVED***"
+
+DEFAULT_LOCATION="Central US"
+DEFAULT_RESOURCEGROUP_NAME="builditSandbox"
+ORGNAME="BuilditAzureSandbox"
+PROJECTNAME="BuilditAzureRig"
+PROJECTID="7b202733-9515-4424-be96-5fb9e2ccc0f3"
+GITORG="buildit"
+GITREPO="digitalrig-metal-azure"
+
+
+
+# Read the parameter values from the command line.
+echo
+echo "Please fill in the config settings to store in your parameters.json"
+echo "Defaults are shown in parenthesis.  <Enter> to accept."
+echo
+
+read -p "Location (\"$DEFAULT_LOCATION\"): " LOCATION
+while [[ -z "$DEVOPSUSERNAME" ]]
+do
+    read -p "Devops Username (normally email with Azure subscription): " DEVOPSUSERNAME
+done
+while [[ -z "$DEVOPSPAT" ]]
+do
+    read -p "Devops Personal Access Token (check readme for instructions to get): " DEVOPSPAT
+done
+USERCRED=$DEVOPSUSERNAME:$DEVOPSPAT
+while [[ -z "$GITPAT" ]]
+do
+    read -p "Github Personal Access Token (check readme for instructions to get): " GITPAT
+done
+read -p "Base Resource Name (\"$DEFAULT_RESOURCEGROUP_NAME\"): " RESOURCEGROUP_NAME
+HASH=$( head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6 ; echo '')
+RESOURCEGROUP_NAME=$DEFAULT_RESOURCEGROUP_NAME$HASH
+
+# read -p "sites_RigContainerApp_name (\"$DEFAULT_CONTAINER_APP_NAME\"): " CONTAINER_APP_NAME
+# read -p "serverfarms_RigContainerPlan_name (\"$DEFAULT_CONTAINER_PLAN_NAME\"): " CONTAINER_PLAN_NAME
+# read -p "registries_RigContainerRegistry_name (\"$DEFAULT_CONTAINER_REGISTRY_NAME\"): " CONTAINER_REGISTRY_NAME
+# read -p "config_web_name (\"$DEFAULT_WEB_CONFIG_NAME\"): " WEB_CONFIG_NAME
+# read -p "hostNameBindings (\"$DEFAULT_HOST_NAME_BINDING\"): " HOST_NAME_BINDING
+# read -p "appInsightsName (\"$DEFAULT_APP_INSIGHTS_NAME\"): " APP_INSIGHTS_NAME
+
+echo
+
+# read -p "location (\"$DEFAULT_DATABASE_LOCATION_NAME\"): " DATABASE_LOCATION_NAME
+# read -p "serverName (\"$DEFAULT_DATABASE_SERVER_NAME\"): " DATABASE_SERVER_NAME
+# read -p "databaseName (\"$DEFAULT_DATABASE_DB_NAME\"): " DATABASE_DB_NAME
+# read -p "administratorLogin (\"$DEFAULT_DATABASE_ADMIN_USERNAME\"): " DATABASE_ADMIN_USERNAME
+# read -p "administratorLoginPassword (\"$DEFAULT_DATABASE_ADMIN_PASSWORD\"): " DATABASE_ADMIN_PASSWORD
+echo
+
+
+# Assign the default values if the user didn't enter any value.
+CONTAINER_APP_NAME="${CONTAINER_APP_NAME:-$DEFAULT_CONTAINER_APP_NAME}"
+CONTAINER_PLAN_NAME="${CONTAINER_PLAN_NAME:-$DEFAULT_CONTAINER_PLAN_NAME}"
+CONTAINER_REGISTRY_NAME="${CONTAINER_REGISTRY_NAME:-$DEFAULT_CONTAINER_REGISTRY_NAME}"
+WEB_CONFIG_NAME="${WEB_CONFIG_NAME:-$DEFAULT_WEB_CONFIG_NAME}"
+HOST_NAME_BINDING="${HOST_NAME_BINDING:-$DEFAULT_HOST_NAME_BINDING}"
+APP_INSIGHTS_NAME="${APP_INSIGHTS_NAME:-$DEFAULT_APP_INSIGHTS_NAME}"
+LOCATION="${LOCATION:-$DEFAULT_LOCATION}"
+
+# DATABASE_LOCATION_NAME="${DATABASE_LOCATION_NAME:-$DEFAULT_DATABASE_LOCATION_NAME}"
+# DATABASE_SERVER_NAME="${DATABASE_SERVER_NAME:-$DEFAULT_DATABASE_SERVER_NAME}"
+# DATABASE_DB_NAME="${DATABASE_DB_NAME:-$DEFAULT_DATABASE_DB_NAME}"
+# DATABASE_ADMIN_USERNAME="${DATABASE_ADMIN_USERNAME:-$DEFAULT_DATABASE_ADMIN_USERNAME}"
+# DATABASE_ADMIN_PASSWORD="${DATABASE_ADMIN_PASSWORD:-$DEFAULT_DATABASE_ADMIN_PASSWORD}"
+
+
+# Build the parameters.json file using the above parameters.
+PARAM_FILE="output/parameters.json"
+cp templates/parameters.json $PARAM_FILE
+
+sed -i "s/CONTAINER_APP_NAME/${CONTAINER_APP_NAME}/g" $PARAM_FILE
+sed -i "s/CONTAINER_PLAN_NAME/${CONTAINER_PLAN_NAME}/g" $PARAM_FILE
+sed -i "s/CONTAINER_REGISTRY_NAME/${CONTAINER_REGISTRY_NAME}/g" $PARAM_FILE
+sed -i "s/WEB_CONFIG_NAME/${WEB_CONFIG_NAME}/g" $PARAM_FILE
+sed -i "s/HOST_NAME_BINDING/${HOST_NAME_BINDING}/g" $PARAM_FILE
+sed -i "s/APP_INSIGHTS_NAME/${APP_INSIGHTS_NAME}/g" $PARAM_FILE
+
+# sed -i '' "s/DATABASE_LOCATION_NAME/${DATABASE_LOCATION_NAME}/g" $PARAM_FILE
+# sed -i '' "s/DATABASE_SERVER_NAME/${DATABASE_SERVER_NAME}/g" $PARAM_FILE
+# sed -i '' "s/DATABASE_DB_NAME/${DATABASE_DB_NAME}/g" $PARAM_FILE
+# sed -i '' "s/DATABASE_ADMIN_USERNAME/${DATABASE_ADMIN_USERNAME}/g" $PARAM_FILE
+# sed -i '' "s/DATABASE_ADMIN_PASSWORD/${DATABASE_ADMIN_PASSWORD}/g" $PARAM_FILE
+
+sed -i "s/LOCATION/${LOCATION}/g" $PARAM_FILE
+sed -i "s/RESOURCEGROUP_NAME/${RESOURCEGROUP_NAME}/g" $PARAM_FILE
+sed -i "s/ORGNAME/${ORGNAME}/g" $PARAM_FILE
+sed -i "s/PROJECTNAME/${PROJECTNAME}/g" $PARAM_FILE
+sed -i "s/USERCRED/${USERCRED}/g" $PARAM_FILE
+sed -i "s/GITORG/${GITORG}/g" $PARAM_FILE
+sed -i "s/GITREPO/${GITREPO}/g" $PARAM_FILE
+sed -i "s/GITPAT/${GITPAT}/g" $PARAM_FILE
+
+# Display an output message and end the script.
+echo "Saved parameters.json!"
+echo
