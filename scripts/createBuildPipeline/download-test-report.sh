641ecd123d8c65691e286985e55481401231808e
Alfredo Figueroa <alfredo.figueroa@wipro.com>
2019-03-12 08:36:59 -0500

    Add the WorkingDirectory to the download-test-report script

--- scripts/createBuildPipeline/download-test-report.sh
+++ scripts/createBuildPipeline/download-test-report.sh
@@ -1,10 +1,11 @@
 export AZURE_STORAGE_ACCOUNT=$1
 export AZURE_STORAGE_KEY=$2
 export BUILD_NUMBER=$3
+export WORKING_DIRECTORY=$4
 
 export container_name=test-results
 export blob_name="TestReport_${BUILD_NUMBER}.xml"
-export destination_file=$(System.DefaultWorkingDirectory)/TestReport.xml
+export destination_file=${WORKING_DIRECTORY}/TestReport.xml
 
 echo "Downloading the file..."
 az storage blob download --container-name $container_name --name $blob_name --file $destination_file --output table
