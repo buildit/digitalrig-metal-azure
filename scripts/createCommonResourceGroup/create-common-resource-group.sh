5d6aa64be89f6e50443f4d190c6c3087122858eb
David Burkhalter <d2.burkhalter@gmail.com>
2019-03-13 15:06:17 -0500

    made create-resource-group.sh redunant

--- scripts/createCommonResourceGroup/create-common-resource-group.sh
+++ scripts/createCommonResourceGroup/create-common-resource-group.sh
@@ -13,26 +13,26 @@ mkdir -p $TEMPLATEPATH
 [ -e $OUTPUTPATH/createCommonResourceGroupOutput.json ] && rm $OUTPUTPATH/createCommonResourceGroupOutput.json
 cp $TEMPLATEPATH/createCommonResourceGroupTemplate.json $DATAPATH/createCommonResourceGroupData.json
 
-
-# Get parameter values from the parameters.json file and inject them into the data file.
-SUBSCRIPTION_ID=$(jq -r '.parameters.subscription_id.value' < ./output/parameters.json)
-RESOURCEGROUP_NAME=$(jq -r '.parameters.commonResourceGroupName.value' < ./output/parameters.json)
-RESOURCEGROUP_LOCATION=$(jq -r '.parameters.commonResourceGroupLocation.value' < ./output/parameters.json)
-
-PARAM_FILE="${DATAPATH}/createCommonResourceGroupData.json"
-LOWER_RESOURCEGROUP_NAME=$(echo "$RESOURCEGROUP_NAME" | awk '{print tolower($0)}')
-COMMON_STORAGEACCOUNT_NAME="${LOWER_RESOURCEGROUP_NAME}stgacc"
-COMMON_CONTAINERREGISTRY_NAME="${LOWER_RESOURCEGROUP_NAME}containerregistry"
-COMMON_RESOURCEGROUP_LOCATION="${RESOURCEGROUP_LOCATION}"
-
-sed -i'' -e "s/COMMON_STORAGEACCOUNT_NAME/${COMMON_STORAGEACCOUNT_NAME}/g" $PARAM_FILE
-sed -i'' -e "s/COMMON_CONTAINERREGISTRY_NAME/${COMMON_CONTAINERREGISTRY_NAME}/g" $PARAM_FILE
-sed -i'' -e "s/COMMON_RESOURCEGROUP_LOCATION/${COMMON_RESOURCEGROUP_LOCATION}/g" $PARAM_FILE
-
-
 # Login to azure using your credentials.
 LOGININFO=$(az account show)
 if [[ -z $LOGININFO ]]; then LOGININFO=$(az login); fi
+
+SUBSCRIPTIONID=$(echo "$LOGININFO" | jq -r '.id')
+SUBSCRIPTIONNAME=$(echo "$LOGININFO" | jq -r '.name')
+TENANTID=$(echo "$LOGININFO" | jq -r '.tenantId')
+
+# Get parameter values from the parameters.json file and inject them into the data file.
+RESOURCEGROUP_NAME=$(jq -r '.parameters.resourceGroupName.value' < ./output/parameters.json)
+RESOURCEGROUP_LOCATION=$(jq -r '.parameters.location.value' < ./output/parameters.json)
+
+DATA_FILE="${DATAPATH}/createCommonResourceGroupData.json"
+LOWER_RESOURCEGROUP_NAME=$(echo "$RESOURCEGROUP_NAME" | awk '{print tolower($0)}')
+COMMON_STORAGEACCOUNT_NAME="${LOWER_RESOURCEGROUP_NAME}stgacc"
+COMMON_CONTAINERREGISTRY_NAME="${LOWER_RESOURCEGROUP_NAME}acr"
+
+sed -i'' -e "s/COMMON_STORAGEACCOUNT_NAME/${COMMON_STORAGEACCOUNT_NAME}/g" $DATA_FILE
+sed -i'' -e "s/COMMON_CONTAINERREGISTRY_NAME/${COMMON_CONTAINERREGISTRY_NAME}/g" $DATA_FILE
+sed -i'' -e "s/COMMON_RESOURCEGROUP_LOCATION/${RESOURCEGROUP_LOCATION}/g" $DATA_FILE
 
 
 # Create or get info for Resource Group.
@@ -44,7 +44,7 @@ RESOURCEGROUPID=$(echo "$RESOURCEINFO" | jq -r '.id')
 az group deployment create \
     --name "${RESOURCEGROUP_NAME}Deployment001" \
     --resource-group "$RESOURCEGROUP_NAME" \
-    --template-file "$PARAM_FILE"
+    --template-file "$DATA_FILE"
 
 
 # Gets the Storage Account Key
@@ -53,14 +53,13 @@ STORAGEACCOUNT_KEYINFO=$(az storage account keys list \
     --account-name "$COMMON_STORAGEACCOUNT_NAME")
 COMMON_STORAGEACCOUNT_KEY=$(echo $STORAGEACCOUNT_KEYINFO | jq -r '.[] | select(.keyName == "key1") | .value')
 
-
 # Creates a new storage blob container.
 CONTAINER_NAME="test-results"
 az storage container create \
     --name $CONTAINER_NAME \
     --account-key $COMMON_STORAGEACCOUNT_KEY \
     --account-name $COMMON_STORAGEACCOUNT_NAME \
-    --subscription $SUBSCRIPTION_ID
+    --subscription $SUBSCRIPTIONID
 
 
 # Saves the container URL and the storage account key in the parameters.json file.
@@ -68,5 +67,10 @@ PARAM_FILE="output/parameters.json"
 COMMON_STORAGEACCOUNT_CONTAINER_URL="https://${COMMON_STORAGEACCOUNT_NAME}.blob.core.windows.net/${CONTAINER_NAME}"
 sed -i'' -e "s~COMMON_STORAGEACCOUNT_KEY~${COMMON_STORAGEACCOUNT_KEY}~g" $PARAM_FILE
 sed -i'' -e "s~COMMON_STORAGEACCOUNT_CONTAINER_URL~${COMMON_STORAGEACCOUNT_CONTAINER_URL}~g" $PARAM_FILE
+
+sed -i'' -e "s/SUBSCRIPTIONID/${SUBSCRIPTIONID}/g" $PARAM_FILE
+sed -i'' -e "s/SUBSCRIPTIONNAME/${SUBSCRIPTIONNAME}/g" $PARAM_FILE
+sed -i'' -e "s/TENANTID/${TENANTID}/g" $PARAM_FILE
+sed -i'' -e "s|\${RESOURCEGROUPID}|$RESOURCEGROUPID|" $PARAM_FILE
 
 echo "Common Resource Group ${RESOURCEGROUP_NAME} created or exists!"
