b1da6d96a3daee4058dec78098f57fd79b6fc57e
Nicholas Galazzo <nicholasgalazzo@Galazzo.local>
2019-03-19 10:56:01 -0400

    Added a new createresource.sh with app insights resource

---
+++ scripts/createReleasePipeline/bashScripts/CreateResources.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+az appservice plan create --resource-group $1 \
+    --name $2-plan \
+    --is-linux \
+    --sku B1
+
+az webapp create --resource-group $1 \
+    --plan $2-plan \
+    --name $2 \
+    --runtime "python|2.7"
+
+az webapp config container set --resource-group $1 \
+    --name $2 \
+    --docker-custom-image-name $3/$4 \
+    --docker-registry-server-url https://$3
+
+az resource create \
+    --resource-group $1 \
+    --resource-type "Microsoft.Insights/components" \
+    --name $2-appinsights \
+    --location eastus\
+    --properties '{"Application_Type":"web"}'
