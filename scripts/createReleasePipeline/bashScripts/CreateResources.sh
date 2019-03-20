336c8a8967df66cad5fef2c9f6ea69093e846c39
Nicholas Galazzo <nicholasgalazzo@Galazzo.local>
2019-03-20 16:57:08 -0400

    ARF-53 fix to syntax in createResources.sh

--- scripts/createReleasePipeline/bashScripts/CreateResources.sh
+++ scripts/createReleasePipeline/bashScripts/CreateResources.sh
@@ -27,13 +27,13 @@ az webapp config container set --resource-group $1 \
     --docker-registry-server-url https://$3
 
 #Get App Insights Instrumentation Key
-INSTRUMENTATION_KEY="$(az resource show -g AzureRig -n RigInsights --resource-type "Microsoft.Insights/components" --query properties.InstrumentationKey)"
+INSTRUMENTATION_KEY="$(az resource show -g $1 -n $2-appinsights --resource-type "Microsoft.Insights/components" --query properties.InstrumentationKey)"
 
 #Add Instrumentation key to 
 az webapp config appsettings set \
     --resource-group $1 \
     --name $2 \ 
-    --settings InstrumentationKey=$INSTRUMENTATION_KEY \
+    --settings InstrumentationKey=$INSTRUMENTATION_KEY 
 
 
 
