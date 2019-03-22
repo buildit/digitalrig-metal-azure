fd77591e3e58523414d8d6280c6a81cc70872fe8
Andrew Ochsner <aochsner@cs.stanford.edu>
2019-03-22 14:38:38 -0500

    Attempt to add logs to app insights
    ARI-53

--- slackbot/pkg/config/config.go
+++ slackbot/pkg/config/config.go
@@ -7,15 +7,27 @@ import (
 
 // Config contains environment variables used to configure the app
 type Config struct {
-	VerificationToken string `envconfig:"SLACKBOT_VERIFICATIONTOKEN"`
-	OauthToken        string `envconfig:"SLACKBOT_OAUTHTOKEN"`
+	VerificationToken string
+	OauthToken        string
 	PollBucket        string `default:"POLL"`
 }
 
+type AppInsightsConfig struct {
+	InstrumentationKey string `default:2b55b26a-ac69-4fb6-8bb3-c6fe2cb70958`
+}
+
 var Env Config
+var AppInsights AppInsightsConfig
 
 func init() {
 	err := envconfig.Process("slackbot", &Env)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func init() {
+	err := envconfig.Process("appinsights", &AppInsights)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
