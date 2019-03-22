b5d6c9f36f242dbdc5d2c5cd0f416cb53e12686c
Alfredo Figueroa <alfredo.figueroa@wipro.com>
2019-03-21 22:56:44 -0500

    Add docker file to run integration tests

---
+++ slackbot/pkg/service/service_e2e_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+import "io/ioutil"
+import "testing"
+
+func TestE2ESlackbotHelloWorldMessage(t *testing.T) {
+	// Call the slackbot end point.
+	httpResponse, err := http.Get("https://builditsandboxdemoappdev.azurewebsites.net/")
+	if err != nil { panic(err) }
+
+	// Parse the response body.
+	defer httpResponse.Body.Close()
+	body, err := ioutil.ReadAll(httpResponse.Body)
+	actual := string(body)
+
+	// Assert the result.
+	expected := "Hello from the slackbot"
+	if actual != expected { t.Errorf("Assertion Failed! Expected: %s. Actual: %s", expected, actual) }
+}
